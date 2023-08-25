74e34c5a69a3bdf837f0804f09e34c4cbfd1dda5
simi-goes <110276583+simi-goes@users.noreply.github.com>
2023-08-25 12:34:47 +0100

    Update streamlit_app.py
    how to work with json

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.text(fruityvice_response)
+# New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+streamlit.text(fruityvice_response.json())
 
 
 
